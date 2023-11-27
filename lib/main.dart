d172cfdd19c3d1cefba01411824a71234a018e7d
ashleyyun <kiwi.ah.oui@gmail.com>
2023-11-27 18:25:18 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: const Text('Home')),
+          body: SafeArea(child: Text('Hello world'))),
+    );
+  }
+}
